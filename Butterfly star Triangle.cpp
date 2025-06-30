7382c3eff9c44f2ad3c8a369f9c37b5c9dbd5826
KARAN SINGH PAL <118987213+Karansinghpal@users.noreply.github.com>
2025-06-30 11:31:56 +0530

    Add files via upload

---
+++ Butterfly star Triangle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void space(int n) {
+    // Upper part of the butterfly
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower part of the butterfly
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int x;
+    cout << "Enter Number: ";
+    cin >> x;
+    space(x);
+    return 0;
+}
+
